7cd733f28b021f9f5be037c450c47359aabf0da7
skzxx <rrachmawan1965@gmail.com>
2021-08-20 03:42:49 +0700

    add

---
+++ Main.js
@@ -0,0 +1,6 @@
+{
+  let myName = "Caleb" //primitive
+  let yourName = new String("Claire") //object. also object can ise a method but primitive can't without object
+  console.log(typeof(myName))
+  console.log(typeof(yourName))
+}
